8ef66820632c896ff6338cedba7847fcc69700ba
Bharti <bharatifule10gmail.com>
2023-06-28 11:07:21 +0530

    UC6-Employee Wage

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     // UC-1------
-    void checkEmployeeAttendence()
-    {
+    void checkEmployeeAttendence( ) {
         int IS_FULL_TIME = 1;
         double empCheck = (int)(Math.random() * 2) ;
         if (empCheck == IS_FULL_TIME)
@@ -29,7 +28,7 @@ public class Main {
         System.out.println("Employee Wage " + employeeWage);
     }
 
-    //UC-3-----------------------------
+    //UC-3----------------------------
     void dailyEmployeePartTimeWage() {
         int fullTime = 2;
         int partTime = 1;
@@ -101,6 +100,40 @@ public class Main {
         System.out.println("Total Employee Wage " + monthlyWage);
     }
 
+    //UC-6----------
+    void employeeMonthlyWageWhileloop() {
+
+        int employeeWagePerHour = 20;
+        int workingDaysPerMonth = 20;
+        int employeeHrs = 0;
+        int employeeWage = 0;
+        int totalEmployeeHours=0;
+        int maximumHoursInMonth=100;
+        int totalWorkingDays=0;
+        while(totalEmployeeHours<= maximumHoursInMonth &&
+                totalWorkingDays<workingDaysPerMonth) {
+            totalWorkingDays++; {
+                int employeeCheck=(int)Math.floor(Math.random() *10) %3;
+                switch(employeeCheck) {
+                    case 1 :
+                        employeeHrs=16;
+                        break;
+                    case 2 :
+                        employeeHrs=8;
+                        break;
+                    default:
+                        employeeHrs=0;
+
+                }
+                employeeWage = employeeHrs * employeeWagePerHour;
+
+                totalEmployeeHours += employeeHrs;
+                System.out.println("Day#: " + totalWorkingDays + " EmpHour: " +employeeHrs);
+            }
+            int totalEmployeeWage= totalEmployeeHours * employeeWagePerHour;
+            System.out.println("Total Employee Wage " +totalEmployeeWage);
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -108,8 +141,9 @@ public class Main {
         // Emp.checkEmployeeAttendence();
         //Emp.dailyEmployeeWage();
         // Emp.dailyEmployeePartTimeWage();
-        Emp.employeeMonthlyWage();
+        // Emp.employeeMonthlyWage();
         // System.out.println(Emp.switchcase());
-
+        Emp.employeeMonthlyWageWhileloop();
     }
+
 }
